be1cf606717f34192ab8ef324382a583dd31e8a7
Mohammed Shareef <farhan.shareef@gmail.com>
2019-01-22 09:16:47 -0500

    check-in on progress

---
+++ Tutorial12 - matrices.R
@@ -0,0 +1,31 @@
+Salary
+Games
+MinutesPlayed
+
+matrix() #bends and transforms a vector into a matrix
+rbind() #binds vectors as rows in a matrix
+cbind() #binds vectors as columns in a matrix
+
+#matrix()
+?matrix
+my.data <- 1:20
+my.data
+A <- matrix(my.data,4,5)
+A
+A[2,3]
+B <- matrix(my.data, 4,5, byrow = T)
+B
+B[2,5]
+
+#rbind()
+r1 <- c("I","am","happy")
+r2 <- c("What","a","day")
+r3 <- c(1,2,3)
+C <- rbind(r1,r2,r3)
+C #all values are converted to char strings, because cannot mix datatypes in matrices
+
+#cbind
+c1 <- 1:5
+c2 <- -1:-5
+D <- cbind(c1,c2)
+D
---
+++ Tutorial14 - matrix operations.R
@@ -0,0 +1,14 @@
+Games
+rownames(Games)
+colnames(Games)
+Games["LeBronJames","2012"]
+
+FieldGoals
+round(FieldGoals/Games,1)
+
+round(MinutesPlayed/Games)
+
+#FGs/game
+round(FieldGoals/Games,1)
+#FGs/FGattempt
+round(FieldGoals/FieldGoalAttempts,1)
